8a99b53a119f7fd6fa354a1447b2f0a7d1f6bdc7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-12 14:16:32 +0000

    mupdf 1.27.0
    mupdf: remove patches
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/m/mupdf.rb
+++ Formula/m/mupdf.rb
@@ -1,27 +1,10 @@
 class Mupdf < Formula
   desc "Lightweight PDF and XPS viewer"
   homepage "https://mupdf.com/"
+  url "https://mupdf.com/downloads/archive/mupdf-1.27.0-source.tar.gz"
+  sha256 "ae2442416de499182d37a526c6fa2bacc7a3bed5a888d113ca04844484dfe7c6"
   license "AGPL-3.0-or-later"
   head "git://git.ghostscript.com/mupdf.git", branch: "master"
-
-  stable do
-    url "https://mupdf.com/downloads/archive/mupdf-1.26.11-source.tar.gz"
-    sha256 "eee47fdb64de309124df21081d4a4da4ad0e917824ab2ed68fc8008f6b523979"
-
-    # libclang-20 patches
-    patch do
-      url "https://github.com/ArtifexSoftware/mupdf/commit/df0b5ee3bb9b12d8c57df55d7b41faf1b874a14d.patch?full_index=1"
-      sha256 "6968a8b80221b01cc30d46bf832ecbcba99d75de238c41315be318f2b02ac892"
-    end
-    patch do
-      url "https://github.com/ArtifexSoftware/mupdf/commit/559e45ac8c134712cd8eaee01536ea3841e3a449.patch?full_index=1"
-      sha256 "868c2955cbebcb99b5336c005cbe4a5867f8654cb9b008bd24ae67df84438968"
-    end
-    patch do
-      url "https://github.com/ArtifexSoftware/mupdf/commit/4bbf411898341d3ba30f521a6c137a788793cd45.patch?full_index=1"
-      sha256 "ac2b1c1b6c21626aaf009928262f7c31e407a886b192d276674ddb94672b1d38"
-    end
-  end
 
   livecheck do
     url "https://mupdf.com/releases"
