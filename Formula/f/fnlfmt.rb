219db10bb528006544c716195eb9aca8365e6afc
botantony <antonsm21@gmail.com>
2025-12-22 17:59:57 +0100

    fnlfmt: revision bump (lua 5.5.0)

--- Formula/f/fnlfmt.rb
+++ Formula/f/fnlfmt.rb
@@ -4,6 +4,7 @@ class Fnlfmt < Formula
   url "https://git.sr.ht/~technomancy/fnlfmt/archive/0.3.2.tar.gz"
   sha256 "646c9033481a70c4430ced7397f6bc04b1d214fd35bee1579dd4c7901b81ac94"
   license "LGPL-3.0-or-later"
+  revision 1
 
   bottle do
     sha256 cellar: :any_skip_relocation, all: "31998e471343bbd653c8067f40a91977a52fa02604254654aad53f5ce79e580d"
